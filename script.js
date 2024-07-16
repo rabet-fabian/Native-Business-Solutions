9f59ba2140ca9b2e91cad9f2acb9ce7ff7bc2b16
Fabian Rabet <rabet.fabian@yahoo.com>
2024-07-16 22:12:33 +0100

    changes to navbar, back to top started
    Co-authored-by: florin-zsiros <florin-zsiros@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
 
 document.addEventListener("scroll", function () {
   var scrisapare = document.getElementById("scrisapare");
@@ -19,10 +19,21 @@ document.addEventListener("scroll", function () {
 });
 
 document.addEventListener("scroll", function () {
-  var scrisapare = document.getElementById("scrisapare2");
-  if (window.scrollY > -100) {
+  var scrisapare2 = document.getElementById("scrisapare2");
+  if (window.scrollY > 100) {
     scrisapare2.style.display = "grid";
   } else {
     scrisapare2.style.display = "none";
   }
 });
+
+function scrollWin() {
+  window.scrollTo(0, 0);
+}
+
+window.onscroll = () => {
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    console.log("You are at the end of the page.");
+  }
+};
+const toTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
